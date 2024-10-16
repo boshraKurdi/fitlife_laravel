f5af86ed1e22d97ca032674ab7a077b1a1b6b521
boshra <boshra@gmail.com>
2024-10-16 22:08:05 +0300

    update

--- app/Http/Controllers/GoalPlanLevelController.php
+++ app/Http/Controllers/GoalPlanLevelController.php
@@ -29,7 +29,7 @@ class GoalPlanLevelController extends Controller
      */
     public function store(StoreGoalPlanLevelRequest $request)
     {
-        //
+        return response()->json('s');
     }
 
     /**
